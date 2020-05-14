b3eee0146b8b6affcfa53c3d8710217da45ddb10
CDX <1138224907@qq.com>
2020-05-14 16:05:13 +0800

    new

---
+++ src/main/java/cn/person/springboot/controller/HelloController.java
@@ -0,0 +1,10 @@
+package cn.person.springboot.controller;
+
+/**
+ * @Auther:cdx
+ * @Date:2020-05-14
+ * @Description:cn.person.springboot.controller
+ * @Version:1.0
+ */
+public class HelloController {
+}
